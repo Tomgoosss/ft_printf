6a79691dfe8d816cb3465b03f007b41e2fc95225
Tom Goossens <tgoossen@f0r4s4.codam.nl>
2023-10-30 16:00:11 +0100

    firtgoodpush

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,42 @@
 /*   By: tgoossen <tgoossen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:40:02 by tgoossen          #+#    #+#             */
-/*   Updated: 2023/10/26 14:41:56 by tgoossen         ###   ########.fr       */
+/*   Updated: 2023/10/30 15:59:13 by tgoossen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libftprintf.h"
 
-int ft_printf(const char *, ...)
+static int printchar(int c)
 {
+	write (1, &c, 1);
+	return (c);
+}
+
+int ft_printf(const char *var, ...)
+{
+	va_list args;
+	
+	int i;
+
+	i  = 0;
+	va_start(args, var);
+	while (var[i])
+	{
+		if (var[i] == '%')
+		{
+			
+		}
+	}
+	va_end(args);
+	return (i); 
+}
+
+int main()
+{
+	char test = 'j';
+
+	ft_printf("this is my printf = %c\n", test);
+	printf("this is the real function = %c\n", test);
 	
 }
---
+++ libftprintf.h
@@ -0,0 +1,9 @@
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <libft/libft.h>
+# include <stdlib.h>
+
+int ft_printf(const char *var, ...);
+
+#endif
