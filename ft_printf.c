003261b0d300962f1b75bf5c39292a247eb9be5e
Tom Goossens <tgoossen@f0r4s2.codam.nl>
2023-10-31 18:25:52 +0100

    fixprintf

--- ft_printf.c
+++ ft_printf.c
@@ -6,42 +6,65 @@
 /*   By: tgoossen <tgoossen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:40:02 by tgoossen          #+#    #+#             */
-/*   Updated: 2023/10/30 15:59:13 by tgoossen         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:12:29 by tgoossen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static int printchar(int c)
+int numpick(const char *var, va_list args)
 {
-	write (1, &c, 1);
-	return (c);
+    int i;
+    char c;
+    const char *str;
+    
+    i = 0;
+	while (var[i])
+	{
+        if (var[i] == 'c')
+        {
+            c = va_arg(args, int);
+            return(write(1, &c, 1));
+        }
+        else if (var[i] == 's')
+        {
+            str = va_arg(args, const char *);
+           	return(write(1, str, strlen(str)));
+        }
+		i++;
+	}
+    return(i);
 }
+
+
 
 int ft_printf(const char *var, ...)
 {
-	va_list args;
-	
-	int i;
+    va_list args;
+    int count;
+    
+    int i;
 
-	i  = 0;
-	va_start(args, var);
-	while (var[i])
-	{
-		if (var[i] == '%')
-		{
-			
-		}
-	}
-	va_end(args);
-	return (i); 
+    i  = 0;
+	count = 0;
+    va_start(args, var);
+    while (var[i])
+    {
+        if ( var[i] != '%' && var[i])
+            write(1, &var[i], 1);
+        else
+            count += numpick(&var[i++], args);
+        i++;
+    }
+    va_end(args);
+    return (count); 
 }
 
 int main()
 {
-	char test = 'j';
+    char *test = "loser wouter";
 
-	ft_printf("this is my printf = %c\n", test);
-	printf("this is the real function = %c\n", test);
-	
+    ft_printf("this is my printf = %s\n", test);
+    printf("this is the real function = %s\n", test);
+    
 }
--- libftprintf.h
+++ libftprintf.h
@@ -1,8 +1,9 @@
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include <libft/libft.h>
+# include "libft/libft.h"
 # include <stdlib.h>
+# include <stdarg.h>
 
 int ft_printf(const char *var, ...);
 
